4ae03ccb6f8df61fbac09e722f1571680062c74e
Olaf Matyja <olafmat@gmail.com>
2018-08-16 19:40:21 +0200

    praca domowa

---
+++ src/main/java/methodsVariablesStatics/TrzeciZlistyZ2.java
@@ -0,0 +1,28 @@
+package methodsVariablesStatics;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+/**
+ * @author Agnieszka Matyja
+ */
+public class TrzeciZlistyZ2 {
+
+        List<String> jakasLista = Arrays.asList("elem1", "elem2", "elem3", "elem4");
+
+    public List<String> getJakasLista() {
+        return jakasLista;
+    }
+
+    public void setJakasLista(List<String> jakasLista) {
+        this.jakasLista = jakasLista;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(TrzeciZlistyZ2.get(3));
+        if(List<String jakasLista.size() < 3;
+                System.out.println("Lista jest za krótka");
+    }
+
+
+
+}
